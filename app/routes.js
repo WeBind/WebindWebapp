91d7eb42626fb74d958e26bab0f68b1c2053c7ca
Eliott <eliott.benoit4@gmail.com>
2016-01-24 20:42:39 +0100

    prod & cons handling

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,5 @@
+var builder = require('xmlbuilder');
+
 module.exports = function(app) {
 
 	// error
@@ -8,6 +10,37 @@ module.exports = function(app) {
 	// index
 	app.get('/', function(req, res) {
 	    res.sendfile('./views/index.html');
+	});
+
+	app.post('/api/scenario', function(req, res) {
+		var root = builder.create('Scenario', {version: '1.0', encoding: 'ISO-8859-1'});
+
+		var providers = root.ele('Providers');
+
+		for(var i = 0 ; i < req.body.providers.length ; i++) {
+			var p = providers.ele('Provider');
+			p.ele('id', req.body.providers[i].id);
+			p.ele('Response_length', req.body.providers[i].size);
+			p.ele('Response_time', req.body.providers[i].delay);
+		}
+
+		var consumers = root.ele('Consumers');
+
+		for(var i = 0 ; i < req.body.consumers.length ; i++) {
+			var c = consumers.ele('Consumer');
+			c.ele('id', req.body.consumers[i].id);
+			c.ele('Starting_time', req.body.consumers[i].startingTime);
+			c.ele('Size', req.body.consumers[i].size);
+			c.ele('Duration', req.body.consumers[i].duration);
+			c.ele('Period', req.body.consumers[i].period);
+			c.ele('Provider', req.body.consumers[i].provider);
+		}
+
+		var xmlString = root.end({ pretty: true, indent: '  ', newline: '\n' });
+
+		console.log(xmlString);
+
+		res.status(200).send();
 	});
 
 }
--- public/js/core.js
+++ public/js/core.js
@@ -13,29 +13,41 @@ var webind = angular
 	}]);
 
     // configure our routes
-webind.config(function($routeProvider) {
+webind.config(function($routeProvider, $locationProvider) {
 	$routeProvider
-			.when('/instances', {
-                templateUrl : 'pages/instances.html',
+	        .when('/', {
+                templateUrl : 'pages/first.html',
                 controller : 'mainController',
             })
-	            .when('/prod', {
+
+			.when('/instances', {
+                templateUrl : 'pages/instances.html',
+                controller : 'instancesController',
+            })
+
+	        .when('/prod', {
                 templateUrl : 'pages/prod.html',
                 controller : 'prodController',
             })
-                .when('/', {
-                templateUrl : 'pages/first.html',
-            })
-                .when('/last', {
+            
+            .when('/last', {
                 templateUrl : 'pages/last.html',
             })
-	            .when('/cons', {
+	        
+	        .when('/cons', {
                 templateUrl : 'pages/cons.html',
                 controller : 'consController',
-            })
+            });
+
+	// use the HTML5 History API
+    $locationProvider.html5Mode(true);
 });
 
-webind.controller('mainController', function($scope, localStorageService) {
+webind.controller('mainController', function($scope) {
+
+});
+
+webind.controller('instancesController', function($scope, localStorageService) {
 
 	var consInStore = localStorageService.get('cons');
 	var prodInStore = localStorageService.get('prod');
@@ -57,10 +69,10 @@ webind.controller('mainController', function($scope, localStorageService) {
 	}, true);
 
 	if($scope.cons == [])
-		$scope.cons.push({'id':'cons1', 'name':'Consumer 1', 'startingTime':'0', 'size':'10','period':'1000','duration':'10000','provider':''});
+		$scope.cons.push({'id':'1', 'name':'Consumer 1', 'startingTime':'0', 'size':'10','period':'1000','duration':'10000','provider':''});
 
 	if($scope.prod == [])
-		$scope.prod.push({'id':'prod1', 'name':'Producer 1', 'size':10, 'delay':1000,'techno':'soap-java'});
+		$scope.prod.push({'id':'1', 'name':'Producer 1', 'size':10, 'delay':1000,'techno':'soap-java'});
 
 
 
@@ -71,14 +83,14 @@ webind.controller('mainController', function($scope, localStorageService) {
 
     	if(nbCons > $scope.cons.length) {
 	    	for(var i = $scope.cons.length ; i < nbCons ; i++)
-	        	$scope.cons.push({'id':'cons' + (i+1), 'name':'Consumer ' + (i+1), 'startingTime':'0', 'size':'10','period':'1000','duration':'10000','provider':''});
+	        	$scope.cons.push({'id':(i+1), 'name':'Consumer ' + (i+1), 'startingTime':'0', 'size':'10','period':'1000','duration':'10000','provider':''});
 	    } else {
 	    	$scope.cons.splice(nbCons);
 	    }
 
 	    if(nbProd > $scope.prod.length) {
 	    	for(var i = $scope.prod.length ; i < nbProd ; i++)
-	        	$scope.prod.push({'id':'prod' + (i+1), 'name':'Producer' + (i+1), 'size':10, 'delay':1000,'techno':'soap-java'});
+	        	$scope.prod.push({'id':(i+1), 'name':'Producer' + (i+1), 'size':10, 'delay':1000,'techno':'soap-java'});
 	    } else {
 	    	$scope.prod.splice(nbProd);
 	    }
@@ -104,7 +116,7 @@ webind.controller('prodController', function($scope, localStorageService) {
 	}, true);
 });
 
-webind.controller('consController', function($scope, localStorageService) {
+webind.controller('consController', function($scope, $http, localStorageService) {
 	var consInStore = localStorageService.get('cons');
 	var prodInStore = localStorageService.get('prod');
 
@@ -118,7 +130,48 @@ webind.controller('consController', function($scope, localStorageService) {
 	$scope.$watch('prod', function () {
 		localStorageService.set('prod', $scope.prod);
 	}, true);
+
+	$scope.onLaunchClick = function(){
+
+		var req = {
+ 			method: 'POST',
+ 			url: '/api/scenario',
+ 			headers: {
+   				'Content-Type': 'Application/json'
+ 			},
+ 			data: { 'providers': $scope.prod, 'consumers': $scope.cons}
+		}
+
+		console.log(req);
+
+    	$http(req)
+            .success(function(data){
+				console.log(data);
+            })
+            .error(function(data){
+                console.log('Error: '+data);
+            });
+    }
+
+    $scope.onSaveClick = function() {
+
+    	var data = { 'providers': $scope.prod, 'consumers': $scope.cons}
+		
+		data = JSON.stringify(data, undefined, 2);
+
+		var blob = new Blob([data], {type: 'text/json'}),
+			e = document.createEvent('MouseEvents'),
+			a = document.createElement('a');
+
+			a.download = "scenario.json";
+			a.href = window.URL.createObjectURL(blob);
+			a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+			e.initMouseEvent('click', true, false, window,
+			  0, 0, 0, 0, 0, false, false, false, false, 0, null);
+			a.dispatchEvent(e);
+	};
 });
+
 
 
 

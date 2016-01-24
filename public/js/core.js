036700ebac3dd128d591705fb92ccf760ce92276
Eliott <eliott.benoit4@gmail.com>
2016-01-24 15:32:14 +0100

    lol

--- public/js/core.js
+++ public/js/core.js
@@ -1,22 +1,87 @@
 // public/core.js
 
 // include ngRoute for all our routing needs
-var webind = angular.module('webind', ['ngRoute', 'ngAnimate']);
+var webind = angular
+	.module('webind', [
+		'ngRoute', 
+		'ngAnimate', 
+		'LocalStorageModule'
+	])
+
+	.config(['localStorageServiceProvider', function(localStorageServiceProvider){
+  		localStorageServiceProvider.setPrefix('ls');
+	}]);
 
     // configure our routes
 webind.config(function($routeProvider) {
 	$routeProvider
-	            .when('/', {
+			.when('/instances', {
                 templateUrl : 'pages/instances.html',
+                controller : 'mainController',
             })
 	            .when('/prod', {
                 templateUrl : 'pages/prod.html',
+            })
+                .when('/', {
+                templateUrl : 'pages/first.html',
+            })
+                .when('/last', {
+                templateUrl : 'pages/last.html',
             })
 	            .when('/cons', {
                 templateUrl : 'pages/cons.html',
             })
 });
 
-webind.controller('mainController', function($scope,$http,$rootScope) {
+webind.controller('mainController', function($scope, localStorageService) {
 
+	var consInStore = localStorageService.get('cons');
+	var prodInStore = localStorageService.get('prod');
+
+	$scope.progressBar = 50;
+
+	$scope.cons = consInStore || [];
+	$scope.prod = prodInStore || [];
+
+	$scope.sliderProd = $scope.prod.length;
+	$scope.sliderCons = $scope.cons.length;
+
+	$scope.$watch('cons', function () {
+  		localStorageService.set('cons', $scope.cons);
+	}, true);
+
+	$scope.$watch('prod', function () {
+		localStorageService.set('prod', $scope.prod);
+	}, true);
+
+	if($scope.cons == [])
+		$scope.cons.push({'id':'cons1', 'name':'Consumer 1', 'startingTime':'0', 'size':'10','period':'1000','duration':'10000','provider':''});
+
+	if($scope.prod == [])
+		$scope.prod.push({'id':'prod1', 'name':'Producer 1', 'size':'10', 'delay':'1000','techno':'soap-java'});
+
+
+
+    $scope.onInstancesNext = function(nbCons, nbProd){
+
+    	console.log("cons : " + nbCons);
+    	console.log("prod : " + nbProd);
+
+    	if(nbCons > $scope.cons.length) {
+	    	for(var i = $scope.cons.length ; i < nbCons ; i++)
+	        	$scope.cons.push({'id':'cons' + (i+1), 'name':'Consumer ' + (i+1), 'startingTime':'0', 'size':'10','period':'1000','duration':'10000','provider':''});
+	    } else {
+	    	$scope.cons.splice(nbCons);
+	    }
+
+	    if(nbProd > $scope.prod.length) {
+	    	for(var i = $scope.prod.length ; i < nbProd ; i++)
+	        	$scope.prod.push({'id':'prod' + (i+1), 'name':'Producer' + (i+1), 'size':'10', 'delay':'1000','techno':'soap-java'});
+	    } else {
+	    	$scope.prod.splice(nbProd);
+	    }
+
+	    console.log($scope.cons);
+	    console.log($scope.prod);
+    }
 });
